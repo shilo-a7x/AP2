d5596f4bd7165b0ad97f7dfbc4494dc2e850d297
shilo-a7x <107671225+shilo-a7x@users.noreply.github.com>
2023-05-17 18:00:35 +0300

    crash fix

--- ex_1b/src/components/ChatPage/ContactColumn.js
+++ ex_1b/src/components/ChatPage/ContactColumn.js
@@ -73,7 +73,7 @@ const ContactColumn = ({ setActiveUser, activeUser, currentChat, setCurrentChat 
                                 aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                            <form>
+                            <form onSubmit={addContact}>
                                 <div className="mb-3">
                                     <label htmlFor="name" className="form-label">Contact Username</label>
                                     <input ref={newContact} onKeyDown={checkRegex} type="text" className="form-control" id="name" placeholder="Enter name"
