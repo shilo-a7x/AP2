e08dd83d96ea2ffd56414a6de7c8ff6a211f82d5
shilo-a7x <107671225+shilo-a7x@users.noreply.github.com>
2023-06-20 23:21:49 +0300

    Registration screen with almost all logic

--- ex_3/android-client/app/src/main/java/com/example/manshma/activities/RegisterActivity.java
+++ ex_3/android-client/app/src/main/java/com/example/manshma/activities/RegisterActivity.java
@@ -24,7 +24,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     private ActivityRegisterBinding binding;
     private String base64ProfilePic;
-    
+
     private ActivityResultLauncher<Intent> galleryLauncher;
 
     @Override
